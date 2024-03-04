b918647bd887fd0892c65686c5a329186a51f84e
Moraiene <vladislav.ponamaryov@gmail.com>
2024-03-04 17:19:23 +0200

    container

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,8 @@
 import header from "@/styles/components/header.module.scss";
 import button from "@/styles/components/button.module.scss";
+import container from "@/styles/components/container.module.scss";
 
 
 export const stylesHeader = header;
 export const stylesButton = button;
+export const stylesContainer = container;
