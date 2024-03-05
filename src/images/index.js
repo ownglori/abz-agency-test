f553001d98ce5c56f84b62aef6f1d4a1d923bd8a
Moraiene <vladislav.ponamaryov@gmail.com>
2024-03-05 23:13:26 +0200

    optimization

--- src/images/index.js
+++ src/images/index.js
@@ -1,6 +1,6 @@
 import banner from "@/images/banners/banner.webp";
-import bannerLg from "@/images/banners/banner-sm.webp";
-import bannerMd from "@/images/banners/banner-sm.webp";
+import bannerLg from "@/images/banners/banner-lg.webp";
+import bannerMd from "@/images/banners/banner-md.webp";
 import bannerSm from "@/images/banners/banner-sm.webp";
 
 
